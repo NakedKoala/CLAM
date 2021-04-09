b8ee1e07efc94134a17c43fcfc94f8eb8f9661ac
NakedKoala <sirgarfield.c@gmail.com>
2021-04-09 18:50:54 -0400

    fix typo

--- datasets/dataset_h5.py
+++ datasets/dataset_h5.py
@@ -74,7 +74,7 @@ class Whole_Slide_Bag(Dataset):
 		hdf5_file = h5py.File(self.file_path, "r")
 		dset = hdf5_file['imgs']
 		for name, value in dset.attrs.items():
-			if name == 'patch_sizes': 
+			if name == 'patch_size': 
 				continue
 			print(name, value)
 
